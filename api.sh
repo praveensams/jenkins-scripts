b47004b05f1484d32b67053439bc44817a9090dd
praveensams <praveensams16@gmail.com>
2019-11-16 07:29:11 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -1,5 +1,5 @@
 curl --request POST \
           --url https://api.sendgrid.com/v3/mail/send \
-            --header 'authorization: Bearer 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' \
+            --header 'authorization: Bearer SG.4' \
               --header 'Content-Type: application/json' \
                 --data '{"personalizations": [{"to": [{"email": "praveensams16@gmail.com"}],"cc": [{"email":"sambath.p@extra.com"}, {"email":"noreplyhybris@gmail.com"}, {"email":"pallei.p@extra.com"},{"email":"goshika.k@extra.com"},{"email":"poojary.t@extra.com"},{"email":"kancha.a@extra.com"} ]}], "from": {"email": "praveensams16@gmail.com"},"subject":"Mobile app is not working", "content": [{"type": "text/plain", "value": "Issue with mobile app"}]}'
